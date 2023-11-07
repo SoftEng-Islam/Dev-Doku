bcb9319a817c7402c761fc462f6fd11762fcf0e4
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-11-07 23:36:18 +0200

    Update DokuHeader.vue

--- src/components/DokuHeader.vue
+++ src/components/DokuHeader.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 </script>
 <template lang="pug">
+//- Our Header
 header(class="h-14 flex items-center px-4 bg-[var(--cD1)]")
 	h1(class="text-white") Dev-Doku
 	ul(class="ml-auto px-2 h-8 overflow-hidden rounded-full flex items-center justify-center gap-2 bg-[var(--cD3)]")
