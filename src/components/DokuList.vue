f4e79b43d576087d08dd0c4e04c771f10e71372c
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-11-08 01:11:54 +0200

    update

--- src/components/DokuList.vue
+++ src/components/DokuList.vue
@@ -2,8 +2,8 @@
 </script>
 <template lang="pug">
 //- All Docs List
-div(class="absolute w-52 h-full z-10 p-4 left-0 bg-[var(--cD3)]")
-	div(class="mx-auto")
+div(class="absolute w-52 h-[var(--DokuListsHeight)] z-10 p-4 left-0 bg-[var(--cD3)]")
+	div()
 		input(type="text", placeholder="Search..." value="" , class="w-full h-8 px-2 rounded-md bg-[var(--cD1)] text-white" )
 	div(class="")
 		ul(class="w-full flex  p-5 flex-col justify-center gap-y-4")
--- src/components/DokuPages.vue
+++ src/components/DokuPages.vue
@@ -2,12 +2,12 @@
 </script>
 <template lang="pug">
 //- pages for selected Doc
-div(class="w-52 h-full absolute left-52 bg-[var(--cD2)]")
-	div(class="w-full flex items-center justify-center")
-		input(type="text", placeholder="Search..." value="" , class="h-8 px-2 rounded-md bg-[var(--cD1)] text-white" )
+div(class="absolute w-52 left-52 p-4 h-[var(--DokuListsHeight)]  bg-[var(--cD1)]")
+	div()
+		input(type="text", placeholder="Search..." value="" , class="w-full h-8 px-2 rounded-md bg-[var(--cD3)] text-white" )
 	div(class="")
-		ul(class="w-full flex  p-5 flex-col justify-center gap-y-4")
-			li(class="relative w-full h-7 rounded-md bg-[var(--cD1)] cursor-pointer ")
-			li(class="relative w-full h-7 rounded-md bg-[var(--cD1)] cursor-pointer ")
-	button(type="button" id="closeOrResize" class="w-4 h-11 rounded-tr-lg rounded-br-lg bg-[var(--cD2)] absolute top-1/2 right-[-1rem] translate-y-[-50%]")
+		ul(class="w-full flex p-5 flex-col justify-center gap-y-4")
+			li(class="relative w-full h-7 rounded-md bg-[var(--cD3)] cursor-pointer ")
+			li(class="relative w-full h-7 rounded-md bg-[var(--cD3)] cursor-pointer ")
+	button(type="button" id="closeOrResize" class="w-4 h-11 rounded-tr-lg rounded-br-lg bg-[var(--cD1)] absolute top-1/2 right-[-1rem] translate-y-[-50%]")
 </template>
