afbc75784df98addf54567e45ab3894e7d1fb15e
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-11-07 23:38:57 +0200

    Update DokuHeader.vue

--- src/components/DokuHeader.vue
+++ src/components/DokuHeader.vue
@@ -3,6 +3,7 @@
 <template lang="pug">
 //- Our Header
 header(class="h-14 flex items-center px-4 bg-[var(--cD1)]")
+	//- App Name or The Logo
 	h1(class="text-white") Dev-Doku
 	ul(class="ml-auto px-2 h-8 overflow-hidden rounded-full flex items-center justify-center gap-2 bg-[var(--cD3)]")
 		li(class="w-5 h-5 rounded-full cursor-pointer hover:scale-90 duration-150 bg-gray-500")
