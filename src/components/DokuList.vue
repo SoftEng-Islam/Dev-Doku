4eee8e7bdbfff0697277c1894280ea59542d5410
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-11-08 00:10:02 +0200

    update

--- src/components/DokuList.vue
+++ src/components/DokuList.vue
@@ -2,9 +2,9 @@
 </script>
 <template lang="pug">
 //- All Docs List
-div(class="absolute flex flex-col items-center w-52 h-full z-10 p-4 left-0 bg-[var(--cD3)]")
-	div(class="w-full flex items-center justify-center")
-		input(type="text", placeholder="Search..." value="" , class="h-8 px-2 rounded-md bg-[var(--cD1)] text-white" )
+div(class="absolute w-52 h-full z-10 p-4 left-0 bg-[var(--cD3)]")
+	div(class="mx-auto")
+		input(type="text", placeholder="Search..." value="" , class="w-full h-8 px-2 rounded-md bg-[var(--cD1)] text-white" )
 	div(class="")
 		ul(class="w-full flex  p-5 flex-col justify-center gap-y-4")
 			li(class="relative w-full h-7 rounded-md bg-[var(--cD1)] cursor-pointer ")
