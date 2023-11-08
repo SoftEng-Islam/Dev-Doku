88205c9d98038505224047350902afd44d6775cb
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-11-09 01:20:03 +0200

    update

--- src/components/DokuHeader.vue
+++ src/components/DokuHeader.vue
@@ -34,13 +34,14 @@ function setColorTheme(color: number): void {
 	switch(color) {
 	case 1:
 		rootElement.style.setProperty("--favColor", '--gra');
-		rootElement.style.setProperty("--borderFavColor", 'var(--bor_gra)');
 		// Dark
+		rootElement.style.setProperty("--borderFavColor", 'var(--bor_gra)');
 		rootElement.style.setProperty("--Theme1", 'var(--th_gra1)');
 		rootElement.style.setProperty("--Theme2", 'var(--th_gra2)');
 		rootElement.style.setProperty("--Theme3", 'var(--th_gra3)');
 		rootElement.style.setProperty("--Theme4", 'var(--th_gra4)');
 		// Light
+		rootElement.style.setProperty("--LborderFavColor", 'var(--gra)');
 		rootElement.style.setProperty("--LTheme1", 'var(--Lth_gra1)');
 		rootElement.style.setProperty("--LTheme2", 'var(--Lth_gra2)');
 		rootElement.style.setProperty("--LTheme3", 'var(--Lth_gra3)');
@@ -48,70 +49,75 @@ function setColorTheme(color: number): void {
 		break;
 	case 2:
 		rootElement.style.setProperty("--favColor", '--pur');
-		rootElement.style.setProperty("--borderFavColor", 'var(--bor_pur)');
 		// Dark
+		rootElement.style.setProperty("--borderFavColor", 'var(--bor_pur)');
 		rootElement.style.setProperty("--Theme1", 'var(--th_pur1)');
 		rootElement.style.setProperty("--Theme2", 'var(--th_pur2)');
 		rootElement.style.setProperty("--Theme3", 'var(--th_pur3)');
 		rootElement.style.setProperty("--Theme4", 'var(--th_pur4)');
 		// Light
+		rootElement.style.setProperty("--LborderFavColor", 'var(--pur)');
 		rootElement.style.setProperty("--LTheme1", 'var(--Lth_pur1)');
-		rootElement.style.setProperty("--LTheme2", 'var(--Lth_pur1)');
-		rootElement.style.setProperty("--LTheme3", 'var(--Lth_pur1)');
-		rootElement.style.setProperty("--LTheme4", 'var(--Lth_pur1)');
+		rootElement.style.setProperty("--LTheme2", 'var(--Lth_pur2)');
+		rootElement.style.setProperty("--LTheme3", 'var(--Lth_pur3)');
+		rootElement.style.setProperty("--LTheme4", 'var(--Lth_pur4)');
 		break;
 	case 3:
 		rootElement.style.setProperty("--favColor", '--pin');
-		rootElement.style.setProperty("--borderFavColor", 'var(--bor_pin)');
 		// Dark
+		rootElement.style.setProperty("--borderFavColor", 'var(--bor_pin)');
 		rootElement.style.setProperty("--Theme1", 'var(--th_pin1)');
 		rootElement.style.setProperty("--Theme2", 'var(--th_pin2)');
 		rootElement.style.setProperty("--Theme3", 'var(--th_pin3)');
 		rootElement.style.setProperty("--Theme4", 'var(--th_pin4)');
 		// Light
+		rootElement.style.setProperty("--LborderFavColor", 'var(--pin)');
 		rootElement.style.setProperty("--LTheme1", 'var(--Lth_pin1)');
-		rootElement.style.setProperty("--LTheme2", 'var(--Lth_pin1)');
-		rootElement.style.setProperty("--LTheme3", 'var(--Lth_pin1)');
-		rootElement.style.setProperty("--LTheme4", 'var(--Lth_pin1)');
+		rootElement.style.setProperty("--LTheme2", 'var(--Lth_pin2)');
+		rootElement.style.setProperty("--LTheme3", 'var(--Lth_pin3)');
+		rootElement.style.setProperty("--LTheme4", 'var(--Lth_pin4)');
 		break;
 	case 4:
 		rootElement.style.setProperty("--favColor", '--blu');
-		rootElement.style.setProperty("--borderFavColor", 'var(--bor_blu)');
 		// Dark
+		rootElement.style.setProperty("--borderFavColor", 'var(--bor_blu)');
 		rootElement.style.setProperty("--Theme1", 'var(--th_blu1)');
 		rootElement.style.setProperty("--Theme2", 'var(--th_blu2)');
 		rootElement.style.setProperty("--Theme3", 'var(--th_blu3)');
 		rootElement.style.setProperty("--Theme4", 'var(--th_blu4)');
 		// Light
+		rootElement.style.setProperty("--LborderFavColor", 'var(--blu)');
 		rootElement.style.setProperty("--LTheme1", 'var(--Lth_blu1)');
-		rootElement.style.setProperty("--LTheme2", 'var(--Lth_blu1)');
-		rootElement.style.setProperty("--LTheme3", 'var(--Lth_blu1)');
-		rootElement.style.setProperty("--LTheme4", 'var(--Lth_blu1)');
+		rootElement.style.setProperty("--LTheme2", 'var(--Lth_blu2)');
+		rootElement.style.setProperty("--LTheme3", 'var(--Lth_blu3)');
+		rootElement.style.setProperty("--LTheme4", 'var(--Lth_blu4)');
 
 		break;
 	case 5:
 		rootElement.style.setProperty("--favColor", '--gre');
-		rootElement.style.setProperty("--borderFavColor", 'var(--bor_gre)');
 		// Dark
+		rootElement.style.setProperty("--borderFavColor", 'var(--bor_gre)');
 		rootElement.style.setProperty("--Theme1", 'var(--th_gre1)');
 		rootElement.style.setProperty("--Theme2", 'var(--th_gre2)');
 		rootElement.style.setProperty("--Theme3", 'var(--th_gre3)');
 		rootElement.style.setProperty("--Theme4", 'var(--th_gre4)');
 		// Light
-		rootElement.style.setProperty("--LTheme1", 'var(--Lth_gre4)');
-		rootElement.style.setProperty("--LTheme2", 'var(--Lth_gre4)');
-		rootElement.style.setProperty("--LTheme3", 'var(--Lth_gre4)');
+		rootElement.style.setProperty("--LborderFavColor", 'var(--gre)');
+		rootElement.style.setProperty("--LTheme1", 'var(--Lth_gre1)');
+		rootElement.style.setProperty("--LTheme2", 'var(--Lth_gre2)');
+		rootElement.style.setProperty("--LTheme3", 'var(--Lth_gre3)');
 		rootElement.style.setProperty("--LTheme4", 'var(--Lth_gre4)');
 		break;
 	case 6:
 		rootElement.style.setProperty("--favColor", '--yel');
-		rootElement.style.setProperty("--borderFavColor", 'var(--bor_yel)');
 		// Dark
+		rootElement.style.setProperty("--borderFavColor", 'var(--bor_yel)');
 		rootElement.style.setProperty("--Theme1", 'var(--th_yel1)');
 		rootElement.style.setProperty("--Theme2", 'var(--th_yel2)');
 		rootElement.style.setProperty("--Theme3", 'var(--th_yel3)');
 		rootElement.style.setProperty("--Theme4", 'var(--th_yel4)');
 		// Light
+		rootElement.style.setProperty("--LborderFavColor", 'var(--yel)');
 		rootElement.style.setProperty("--LTheme1", 'var(--Lth_yel1)');
 		rootElement.style.setProperty("--LTheme2", 'var(--Lth_yel2)');
 		rootElement.style.setProperty("--LTheme3", 'var(--Lth_yel3)');
@@ -127,11 +133,11 @@ header(class="overflow-hidden w-full top-0 left-0 h-[var(--HeaderHeight)] flex i
 
 
 	//- Dark / Light Button
-	div(class="toggleMode ml-auto h-7 w-16 mx-3 flex flex-row items-center justify-center rounded-full  bg-[var(--Theme4)] scale-95" @click="darkModeChecked = !darkModeChecked; MT_toggleMode();")
+	div(class="toggleMode ml-auto h-7 w-16 mx-3 flex flex-row items-center justify-center rounded-full bg-[var(--Theme4)] scale-95" @click="darkModeChecked = !darkModeChecked; MT_toggleMode();")
 		input(type="checkbox" :checked="darkModeChecked")
 		label(for="darkmode-toggle")
-			<svg viewBox="0 0 24 24" class="sun" xmlns="http://www.w3.org/2000/svg"><path d="M12 18C8.68629 18 6 15.3137 6 12C6 8.68629 8.68629 6 12 6C15.3137 6 18 8.68629 18 12C18 15.3137 15.3137 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16ZM11 1H13V4H11V1ZM11 20H13V23H11V20ZM3.51472 4.92893L4.92893 3.51472L7.05025 5.63604L5.63604 7.05025L3.51472 4.92893ZM16.9497 18.364L18.364 16.9497L20.4853 19.0711L19.0711 20.4853L16.9497 18.364ZM19.0711 3.51472L20.4853 4.92893L18.364 7.05025L16.9497 5.63604L19.0711 3.51472ZM5.63604 16.9497L7.05025 18.364L4.92893 20.4853L3.51472 19.0711L5.63604 16.9497ZM23 11V13H20V11H23ZM4 11V13H1V11H4Z"/></svg>
-			<svg viewBox="0 0 24 24" class="moon" xmlns="http://www.w3.org/2000/svg"><path d="M10 7C10 10.866 13.134 14 17 14C18.9584 14 20.729 13.1957 21.9995 11.8995C22 11.933 22 11.9665 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C12.0335 2 12.067 2 12.1005 2.00049C10.8043 3.27098 10 5.04157 10 7ZM4 12C4 16.4183 7.58172 20 12 20C15.0583 20 17.7158 18.2839 19.062 15.7621C18.3945 15.9187 17.7035 16 17 16C12.0294 16 8 11.9706 8 7C8 6.29648 8.08133 5.60547 8.2379 4.938C5.71611 6.28423 4 8.9417 4 12Z"/></svg>
+			<svg viewBox="0 0 24 24" class="sun pl-[3px]" xmlns="http://www.w3.org/2000/svg"><path d="M12 18C8.68629 18 6 15.3137 6 12C6 8.68629 8.68629 6 12 6C15.3137 6 18 8.68629 18 12C18 15.3137 15.3137 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16ZM11 1H13V4H11V1ZM11 20H13V23H11V20ZM3.51472 4.92893L4.92893 3.51472L7.05025 5.63604L5.63604 7.05025L3.51472 4.92893ZM16.9497 18.364L18.364 16.9497L20.4853 19.0711L19.0711 20.4853L16.9497 18.364ZM19.0711 3.51472L20.4853 4.92893L18.364 7.05025L16.9497 5.63604L19.0711 3.51472ZM5.63604 16.9497L7.05025 18.364L4.92893 20.4853L3.51472 19.0711L5.63604 16.9497ZM23 11V13H20V11H23ZM4 11V13H1V11H4Z"/></svg>
+			<svg viewBox="0 0 24 24" class="moon pb-[2px]" xmlns="http://www.w3.org/2000/svg"><path d="M10 7C10 10.866 13.134 14 17 14C18.9584 14 20.729 13.1957 21.9995 11.8995C22 11.933 22 11.9665 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C12.0335 2 12.067 2 12.1005 2.00049C10.8043 3.27098 10 5.04157 10 7ZM4 12C4 16.4183 7.58172 20 12 20C15.0583 20 17.7158 18.2839 19.062 15.7621C18.3945 15.9187 17.7035 16 17 16C12.0294 16 8 11.9706 8 7C8 6.29648 8.08133 5.60547 8.2379 4.938C5.71611 6.28423 4 8.9417 4 12Z"/></svg>
 
 	//- Change App Colors and Theme
 	div(class="px-2 h-8 overflow-hidden rounded-full flex items-center justify-center gap-2 bg-[var(--Theme3)]")
