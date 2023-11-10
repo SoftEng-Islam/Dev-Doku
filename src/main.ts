86e5e10427be2bf55b23afb6bc97eeed1cbec6a0
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-11-10 19:47:04 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
+import { createPinia } from "pinia";
 import { createApp } from "vue";
-import "./styles.css";
-import 'animate.css';
-import 'remixicon/fonts/remixicon.css'
 import App from "./App.vue";
 
+import "./styles.css";
+import 'animate.css';
+import 'remixicon/fonts/remixicon.css';
 
-createApp(App).mount("#app");
+// Minimalistic but perfect custom scrollbar plugin
+import PerfectScrollbar from "vue3-perfect-scrollbar";
+import "vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css";
+
+// Tippy.js is the complete tooltip, popover, dropdown
+import VueTippy from 'vue-tippy';
+import 'tippy.js/dist/tippy.css'; // optional for styling
+
+
+
+
+// const editor = new SimpleAffineEditor();
+// document.body.appendChild(editor);
+
+// mount App
+createApp(App)
+.use(createPinia())
+.use(VueTippy,  {
+	directive: 'tippy', // => v-tippy
+	component: 'tippy', // => <tippy/>
+	componentSingleton: 'tippy-singleton', // => <tippy-singleton/>,
+	defaultProps: {
+	  placement: 'auto-end',
+	  allowHTML: true,
+	}, // => Global default options * see all props
+  }).use(PerfectScrollbar, {
+	watchOptions: true,
+	options: {
+		swipeEasing: true,
+	},
+}).mount("#app");
