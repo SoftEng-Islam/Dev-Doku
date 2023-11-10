ab6fc6419e49a84b70c106cb33574251459babf1
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-11-10 02:37:46 +0200

    update icons

--- src/components/DokuHeader.vue
+++ src/components/DokuHeader.vue
@@ -150,19 +150,16 @@ header(class="overflow-hidden w-full top-0 left-0 h-[var(--HeaderHeight)] flex i
 	div(class="toggleMode h-7 w-16 mx-3 flex flex-row items-center justify-center rounded-full bg-[var(--Theme4)] scale-95" @click="darkModeChecked = !darkModeChecked; MT_toggleMode();")
 		input(type="checkbox" :checked="darkModeChecked")
 		label(for="darkmode-toggle")
-			<svg viewBox="0 0 24 24" class="sun pl-[3px]" xmlns="http://www.w3.org/2000/svg"><path d="M12 18C8.68629 18 6 15.3137 6 12C6 8.68629 8.68629 6 12 6C15.3137 6 18 8.68629 18 12C18 15.3137 15.3137 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16ZM11 1H13V4H11V1ZM11 20H13V23H11V20ZM3.51472 4.92893L4.92893 3.51472L7.05025 5.63604L5.63604 7.05025L3.51472 4.92893ZM16.9497 18.364L18.364 16.9497L20.4853 19.0711L19.0711 20.4853L16.9497 18.364ZM19.0711 3.51472L20.4853 4.92893L18.364 7.05025L16.9497 5.63604L19.0711 3.51472ZM5.63604 16.9497L7.05025 18.364L4.92893 20.4853L3.51472 19.0711L5.63604 16.9497ZM23 11V13H20V11H23ZM4 11V13H1V11H4Z"/></svg>
-			<svg viewBox="0 0 24 24" class="moon pb-[2px]" xmlns="http://www.w3.org/2000/svg"><path d="M10 7C10 10.866 13.134 14 17 14C18.9584 14 20.729 13.1957 21.9995 11.8995C22 11.933 22 11.9665 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C12.0335 2 12.067 2 12.1005 2.00049C10.8043 3.27098 10 5.04157 10 7ZM4 12C4 16.4183 7.58172 20 12 20C15.0583 20 17.7158 18.2839 19.062 15.7621C18.3945 15.9187 17.7035 16 17 16C12.0294 16 8 11.9706 8 7C8 6.29648 8.08133 5.60547 8.2379 4.938C5.71611 6.28423 4 8.9417 4 12Z"/></svg>
-
-
+			<i class="sun pl-[3px] ri-sun-fill ri-xl text-white"></i>
+			<i class="moon ri-moon-fill pb-[1px] ri-xl text-white"></i>
 	//- titlebar buttons
 	div(class="flex items-center justify-center gap-2 ml-4")
-		button(type="button" @click="minimize" class="w-6 h-6 rounded-md cursor-pointer hover:opacity-50 bg-yellow-500")
-			<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="white"><path d="M12 16L6 10H18L12 16Z"/></svg>
-		button(type="button" @click="maximize" class="w-6 h-6 rounded-md cursor-pointer hover:opacity-50 bg-green-500")
-			<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="white"><path d="M12 8L18 14H6L12 8Z"/></svg>
-		button(type="button" @click="close" class="w-6 h-6 rounded-md cursor-pointer hover:opacity-50 bg-red-500")
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" fill="white" stroke="white" stroke-width="2"><path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/></svg>
-
+		button(type="button" @click="minimize" class="w-6 h-6 flex items-center justify-center rounded-md cursor-pointer hover:opacity-50 bg-yellow-500")
+			<i class="ri-arrow-down-s-fill ri-xl text-white"></i>
+		button(type="button" @click="maximize" class="w-6 h-6 flex items-center justify-center rounded-md cursor-pointer hover:opacity-50 bg-green-500")
+			<i class="ri-arrow-up-s-fill ri-xl text-white"></i>
+		button(type="button" @click="close" class="w-6 h-6 flex items-center justify-center rounded-md cursor-pointer hover:opacity-50 bg-red-500")
+			<i class="ri-close-fill ri-xl text-white"></i>
 </template>
 <style scoped lang="scss">
 .toggleMode {
@@ -177,10 +174,10 @@ header(class="overflow-hidden w-full top-0 left-0 h-[var(--HeaderHeight)] flex i
 			transform: translateX(-100%);
 			background: linear-gradient(180deg,#868686,#8D5EB9);
 		}
-		&:checked + label svg.sun {
+		&:checked + label i.sun {
 			@apply fill-gray-400
 		}
-		&:checked + label svg.moon {
+		&:checked + label i.moon {
 			@apply fill-white;
 		}
 	}
@@ -193,13 +190,13 @@ header(class="overflow-hidden w-full top-0 left-0 h-[var(--HeaderHeight)] flex i
 			background: linear-gradient(180deg,#ffcc89,#c3790c);
 			box-shadow: 0px 5px 10px rgba(0,0,0,0.2);
 		}
-		& svg {
+		& i {
 			@apply absolute w-7 z-10;
 		}
-		& svg.sun {
+		& i.sun {
 			@apply left-0 fill-white duration-300;
 		}
-		& svg.moon {
+		& i.moon {
 			@apply  right-0 fill-gray-400 duration-300;
 		}
 		&:active:after{
