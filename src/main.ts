9637857bd4f12a0a5295eb4359e05118386d4c43
Islam Ahmed <softeng.islam@gmail.com>
2024-08-09 16:52:18 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -3,32 +3,32 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "./styles.css";
-import 'animate.css';
-import 'remixicon/fonts/remixicon.css';
+import "animate.css";
+import "remixicon/fonts/remixicon.css";
 
 // Minimalistic but perfect custom scrollbar plugin
-import PerfectScrollbar from "vue3-perfect-scrollbar";
-import "vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css";
+import { PerfectScrollbar } from "vue3-perfect-scrollbar";
+import "./styles/PerfectScrollbar.css";
 
 // Tippy.js is the complete tooltip, popover, dropdown
-import VueTippy from 'vue-tippy';
-import 'tippy.js/dist/tippy.css'; // optional for styling
-
+import VueTippy from "vue-tippy";
 
 // mount App
 createApp(App)
-.use(createPinia())
-.use(VueTippy,  {
-	directive: 'tippy', // => v-tippy
-	component: 'tippy', // => <tippy/>
-	componentSingleton: 'tippy-singleton', // => <tippy-singleton/>,
-	defaultProps: {
-	  placement: 'auto-end',
-	  allowHTML: true,
-	}, // => Global default options * see all props
-  }).use(PerfectScrollbar, {
-	watchOptions: true,
-	options: {
-		swipeEasing: true,
-	},
-}).mount("#app");
+	.use(createPinia())
+	.use(VueTippy, {
+		directive: "tippy", // => v-tippy
+		component: "tippy", // => <tippy/>
+		componentSingleton: "tippy-singleton", // => <tippy-singleton/>,
+		defaultProps: {
+			placement: "auto-end",
+			allowHTML: true,
+		}, // => Global default options * see all props
+	})
+	.use(PerfectScrollbar, {
+		watchOptions: true,
+		options: {
+			swipeEasing: true,
+		},
+	})
+	.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -5,29 +5,29 @@ import pugPlugin from "vite-plugin-pug";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [
-	pugPlugin(),
-	vue(
-		{
-		template: {
-			preprocessOptions: {
-				plugins: [vuePugPlugin],
-			},
-		},
-	}),
+	plugins: [
+		pugPlugin(),
+		vue(
+			{
+				template: {
+					preprocessOptions: {
+						plugins: [vuePugPlugin],
+					},
+				},
+			}),
 
-],
+	],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-  },
-  // 3. to make use of `TAURI_DEBUG` and other env variables
-  // https://tauri.app/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ["VITE_", "TAURI_"],
+	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+	//
+	// 1. prevent vite from obscuring rust errors
+	clearScreen: false,
+	// 2. tauri expects a fixed port, fail if that port is not available
+	server: {
+		port: 1420,
+		strictPort: false,
+	},
+	// 3. to make use of `TAURI_DEBUG` and other env variables
+	// https://tauri.app/v1/api/config#buildconfig.beforedevcommand
+	envPrefix: ["VITE_", "TAURI_"],
 }));
